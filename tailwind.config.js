87a0d00957b70bd35e3061cc78a4d299ce017536
MahdyMusave <s.mosave13@gmail.com>
2024-07-03 03:16:48 +0200

    typeWrite Effect 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,34 +4,59 @@ module.exports = {
   
   theme: {
     extend: {
-      keyframes:{
-        typing: {
-          "0%": {
-            width: "0%",
-            visibility: "hidden"
+    //   keyframes:{
+    //     typing: {
+    //       "0%": {
+    //         width: "0%",
+    //         visibility: "hidden"
+    //       },
+    //       "100%": {
+    //         width: "100%"
+    //       }
+    //     },
+    //  blink: {
+    //       "50%": {
+    //         borderColor: "transparent"
+    //       },
+    //       "100%": {
+    //         borderColor: "white"
+    //       }
+    //     }
+    //   },
+
+    //   animation:{
+    //      typing: "typing 2s steps(20) infinite alternate, blink .7s infinite"
+    //   }
+      // type write animation 2
+    animation: {
+      typewriter: 'typewriter 2s steps(11) forwards',
+      caret: 'typewriter 2s steps(11) forwards, blink 1s steps(11) infinite 2s',
+    },    
+    keyframes: {
+        typewriter: {
+          to: {
+            left: '100%',
           },
-          "100%": {
-            width: "100%"
-          }
         },
-     blink: {
-          "50%": {
-            borderColor: "transparent"
+        blink: {
+          '0%': {
+            opacity: '0',
           },
-          "100%": {
-            borderColor: "white"
-          }
-        }
+          '0.1%': {
+            opacity: '1',
+          },
+          '50%': {
+            opacity: '1',
+          },
+          '50.1%': {
+            opacity: '0',
+          },
+          '100%': {
+            opacity: '0',
+          },
+        },
       },
-
-      animation:{
-        // type:"typing 2s steps(20) infinite alternate , blink .7s infinite"
-         typing: "typing 2s steps(20) infinite alternate, blink .7s infinite"
-      }
-      
-
-
-    },
+    }
   },
   plugins: [],
 }
